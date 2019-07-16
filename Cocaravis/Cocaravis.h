2ca0255122de2bcc7fcce61141202857d590da34
illusia, decafish <https://decafish.blog.so-net.ne.jp>
2019-07-16 16:53:15 +0900

    Initial Commit

---
+++ Cocaravis/Cocaravis.h
@@ -0,0 +1,19 @@
+//
+//  Cocaravis.h
+//  Cocaravis
+//
+//  Created by 山田 隆俊 on 2019/07/16.
+//  Copyright © 2019 decafish. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Cocaravis.
+FOUNDATION_EXPORT double CocaravisVersionNumber;
+
+//! Project version string for Cocaravis.
+FOUNDATION_EXPORT const unsigned char CocaravisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cocaravis/PublicHeader.h>
+
+
